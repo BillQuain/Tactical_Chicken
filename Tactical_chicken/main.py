a777269036f5612f15dd256203a201858356bc2c
BillQuain <billyq151515@gmail.com>
2022-11-03 19:38:24 -0500

    updated main and button files

--- Tactical_chicken/main.py
+++ Tactical_chicken/main.py
@@ -2,7 +2,7 @@
 #Bill Quain And Timothy Worthem
 
 import pygame, sys
-
+import button as bt
 
 
 def main():
@@ -10,6 +10,7 @@ def main():
     WINDOW_WIDTH,WINDOW_HEIGHT = 1280,720
     display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
     pygame.display.set_caption("Tactical Chicken by Bill and Tim")
+
 
     font = pygame.font.Font('/Windows/Fonts/Arial.ttf', 100)
     titleText = font.render("TACTICAL CHICKEN", True, (255,255,255))
@@ -22,14 +23,14 @@ def main():
     menuArtGun_surf = pygame.image.load("uzi.png")
     menuArtGun_rect = menuArtGun_surf.get_rect(center = (350,540))
 
-    startButt_surf = pygame.Surface((200,50))
-    startButt_rect = startButt_surf.get_rect(center = (640,280))
+    quit_butt_image = pygame.image.load("quit.png").convert_alpha()
+    start_butt_image = pygame.image.load("StartBtn.png").convert_alpha()
+    sett_butt_image = pygame.image.load("settingsBtn.png").convert_alpha()
 
-    settButt_surf = pygame.Surface((200, 50))
-    settButt_rect = settButt_surf.get_rect(center=(640, 380))
+    quit_button = bt.Button(1150, 640, quit_butt_image, 1)
+    start_button = bt.Button(600,280,start_butt_image,4)
+    settings_button = bt.Button(600,380,sett_butt_image,4)
 
-    quitButt_surf = pygame.Surface((200,50))
-    quitButt_rect = quitButt_surf.get_rect(bottomright = (1280,720))
 
     gameState = True
     while gameState:
@@ -41,15 +42,16 @@ def main():
         clk.tick(60)
 
         display_surface.fill((59,97,30))
-        quitButt_surf.fill((138, 41, 34))
-        startButt_surf.fill((135,115,72))
-        settButt_surf.fill((135,115,72))
         display_surface.blit(menuArt_surf,menuArt_rect)
         display_surface.blit(menuArtGun_surf,menuArtGun_rect)
         display_surface.blit(titleText,titleText_rect)
-        display_surface.blit(startButt_surf,startButt_rect)
-        display_surface.blit(settButt_surf,settButt_rect)
-        display_surface.blit(quitButt_surf,quitButt_rect)
+
+        start_button.draw(display_surface)
+        settings_button.draw(display_surface)
+
+        if quit_button.draw(display_surface) == True:
+            gameState = False
+
         pygame.display.update()
 
     return
