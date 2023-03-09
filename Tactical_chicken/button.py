ac1a0b5ac12639ab1f6d7a51311db272a0d8fd54
BillQuain <billyq151515@gmail.com>
2023-03-09 17:02:15 -0600

    version 1.5 of tactical chicken

--- Tactical_chicken/button.py
+++ Tactical_chicken/button.py
@@ -3,7 +3,7 @@ import pygame
 
 # button class
 
-
+'''
 class Button():
     def __init__(self, x, y, image, scale):
         width = image.get_width()
@@ -31,3 +31,21 @@ class Button():
         surface.blit(self.image, (self.rect.x, self.rect.y))
 
         return action
+'''
+class Button():
+    def __init__(self,image_path,x,y, scale):
+        width = image_path.get_width()
+        height = image_path.get_height()
+        self.image = pygame.transform.scale(image_path, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def draw(self,surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+    def is_clicked(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            return pygame.mouse.get_pressed()[0]
+        return False
--- Tactical_chicken/main.py
+++ Tactical_chicken/main.py
@@ -3,99 +3,195 @@
 
 import pygame, sys
 import button as bt
+from settings import *
+class Game:
+	def __init__(self):
+		pygame.init()
+		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+		pygame.display.set_caption('TACTICAL CHICKEN by Bill And Tim')
+		self.clock = pygame.time.Clock()
+		#TACTICAL CHICKEN title
+		self.title_text = pygame.image.load('tactchicl.png').convert_alpha()
+		self.title_rect = self.title_text.get_rect(center = (720,200))
+		#chicken and uzi art
+		self.menueArt_surf = pygame.image.load("chicky2.png").convert_alpha()
+		self.menueArt_rect = self.menueArt_surf.get_rect(center = (350,540))
+		self.menueArtGun_surf = pygame.image.load("uzi.png").convert_alpha()
+		self.menueArtGun_rect = self.menueArtGun_surf.get_rect(center =(350,540))
+		#button images
+		self.quit_butt_image = pygame.image.load("quit.png").convert_alpha()
+		self.start_butt_image = pygame.image.load("StartBtn.png").convert_alpha()
+		self.set_butt_image = pygame.image.load("settingsBtn.png").convert_alpha()
+		#buttons
+		#(self,image_path,x,y, scale)
+		self.start_button = bt.Button(self.start_butt_image,600,280,4)
+		self.settings_button = bt.Button(self.set_butt_image, 600, 380, 4)
+		self.quit_button = bt.Button(self.quit_butt_image, 1150, 640, 1)
+		#self.start_button = bt.Button(600,280,self.start_butt_image,4)
+		#self.settings_button = bt.Button(600,380,self.set_butt_image,4)
+		#self.quit_button = bt.Button(1150,640,self.quit_but_image,1)
+
+	def startgame(self,surface):
+		surface.fill((160,153,100))
+		background = pygame.image.load("background.png").convert_alpha()
+		background_rect = background.get_rect(center = (640,360))
+		hud = pygame.image.load("gameHUD.png").convert_alpha()
+		hud_rect = hud.get_rect(center=(880, 660))
+
+		surface.blit(background, background_rect)
+		surface.blit(hud, hud_rect)
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_ESCAPE:
+						return
+
+			pygame.display.update()
+
+	def settings(self,surface):
+		running = True
+		while running:
+			surface.fill((0, 0, 0))
+			chicken = pygame.image.load("chicky2.png").convert_alpha()
+			chicken_rect = chicken.get_rect(center=(740, 260))
+
+			surface.blit(chicken, chicken_rect)
+			for event in pygame.event.get():
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_ESCAPE:
+						return
+
+			pygame.display.update()
+
+	def run(self):
+		while True:
+			# event loop
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+			#delta time so game runs at proper speed
+			dt = self.clock.tick() / 1000
+			#fills the background to green
+			self.display_surface.fill((59,97,30))
+			#display menue art and title
+			self.display_surface.blit(self.menueArt_surf,self.menueArt_rect)
+			self.display_surface.blit(self.menueArtGun_surf,self.menueArtGun_rect)
+			self.display_surface.blit(self.title_text,self.title_rect)
+			self.start_button.draw(self.display_surface)
+			self.quit_button.draw(self.display_surface)
+			self.settings_button.draw(self.display_surface)
+			#button code
+			if self.start_button.is_clicked():
+				self.startgame(self.display_surface)
+			if self.settings_button.is_clicked():
+				self.settings(self.display_surface)
+			if self.quit_button.is_clicked():
+				pygame.quit()
+				sys.exit()
 
 
-def main():
-    pygame.init()
-    WINDOW_WIDTH,WINDOW_HEIGHT = 1280,720
-    display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
-    pygame.display.set_caption("Tactical Chicken by Bill and Tim")
 
 
 
-    titleText = pygame.image.load("tactchicl.png").convert_alpha()
-    titleText_rect = titleText.get_rect(center = (720,200))
-    clk = pygame.time.Clock()
-
-    menuArt_surf = pygame.image.load("chicky2.png").convert_alpha()
-    menuArt_rect = menuArt_surf.get_rect(center = (320,540))
-
-    menuArtGun_surf = pygame.image.load("uzi.png")
-    menuArtGun_rect = menuArtGun_surf.get_rect(center = (350,540))
-
-    quit_butt_image = pygame.image.load("quit.png").convert_alpha()
-    start_butt_image = pygame.image.load("StartBtn.png").convert_alpha()
-    sett_butt_image = pygame.image.load("settingsBtn.png").convert_alpha()
-
-    quit_button = bt.Button(1150, 640, quit_butt_image, 1)
-    start_button = bt.Button(600,280,start_butt_image,4)
-    settings_button = bt.Button(600,380,sett_butt_image,4)
 
 
-    gameState = True
-    while gameState:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-        clk.tick(60)
-
-        display_surface.fill((59,97,30))
-
-        display_surface.blit(menuArt_surf,menuArt_rect)
-        display_surface.blit(menuArtGun_surf,menuArtGun_rect)
-        display_surface.blit(titleText,titleText_rect)
-
-        if quit_button.draw(display_surface) == True:
-            gameState = False
-
-        if start_button.draw((display_surface)) ==True:
-            startgame(display_surface)
-
-        if settings_button.draw((display_surface)) == True:
-            settings(display_surface)
+			pygame.display.update()
 
 
-        pygame.display.update()
 
-    return
+'''def main():
+	pygame.init()
+	WINDOW_WIDTH,WINDOW_HEIGHT = 1280,720
+	display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+	pygame.display.set_caption("Tactical Chicken by Bill and Tim")
+
+
+
+	titleText = pygame.image.load("tactchicl.png").convert_alpha()
+	titleText_rect = titleText.get_rect(center = (720,200))
+	clk = pygame.time.Clock()
+
+	menuArt_surf = pygame.image.load("chicky2.png").convert_alpha()
+	menuArt_rect = menuArt_surf.get_rect(center = (320,540))
+
+	menuArtGun_surf = pygame.image.load("uzi.png")
+	menuArtGun_rect = menuArtGun_surf.get_rect(center = (350,540))
+
+	quit_butt_image = pygame.image.load("quit.png").convert_alpha()
+	start_butt_image = pygame.image.load("StartBtn.png").convert_alpha()
+	sett_butt_image = pygame.image.load("settingsBtn.png").convert_alpha()
+
+	quit_button = bt.Button(1150, 640, quit_butt_image, 1)
+	start_button = bt.Button(600,280,start_butt_image,4)
+	settings_button = bt.Button(600,380,sett_butt_image,4)
+
+
+	gameState = True
+	while gameState:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+
+		clk.tick(60)
+
+		display_surface.fill((59,97,30))
+
+		display_surface.blit(menuArt_surf,menuArt_rect)
+		display_surface.blit(menuArtGun_surf,menuArtGun_rect)
+		display_surface.blit(titleText,titleText_rect)
+
+		if quit_button.draw(display_surface) == True:
+			gameState = False
+
+		if start_button.draw((display_surface)) ==True:
+			startgame(display_surface)
+
+		if settings_button.draw((display_surface)) == True:
+			settings(display_surface)
+
+
+		pygame.display.update()
+
+	return
 
 def startgame(surface):
-    running = True
-    while running:
-        surface.fill((160,153,100))
-        background  = pygame.image.load("background.png").convert_alpha()
-        background_rect = background.get_rect(center = (640,360))
-        hud = pygame.image.load("gameHUD.png").convert_alpha()
-        hud_rect = hud.get_rect(center = (880,660))
+	running = True
+	while running:
+		surface.fill((160,153,100))
+		background  = pygame.image.load("background.png").convert_alpha()
+		background_rect = background.get_rect(center = (640,360))
+		hud = pygame.image.load("gameHUD.png").convert_alpha()
+		hud_rect = hud.get_rect(center = (880,660))
 
 
-        surface.blit(background,background_rect)
-        surface.blit(hud,hud_rect)
+		surface.blit(background,background_rect)
+		surface.blit(hud,hud_rect)
 
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    return
+		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					return
 
-        pygame.display.update()
+		pygame.display.update()
 
 
 def settings(surface):
-    running = True
-    while running:
-        surface.fill((0, 0, 0))
-        chicken = pygame.image.load("chicky2.png").convert_alpha()
-        chicken_rect = chicken.get_rect(center=(740, 260))
+	running = True
+	while running:
+		surface.fill((0, 0, 0))
+		chicken = pygame.image.load("chicky2.png").convert_alpha()
+		chicken_rect = chicken.get_rect(center=(740, 260))
 
-        surface.blit(chicken, chicken_rect)
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    return
+		surface.blit(chicken, chicken_rect)
+		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					return
 
-        pygame.display.update()
-
+		pygame.display.update()
+'''
 if __name__ == '__main__':
-    main()
+	game = Game()
+	game.run()
