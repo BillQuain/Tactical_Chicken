105ad26557ce4e782b9a0b690e0b1b0419be961f
Tworthem <114034494+Tworthem@users.noreply.github.com>
2023-03-30 16:31:24 -0500

    Add files via upload

---
+++ Tactical_chicken/City.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="City" tilewidth="8" tileheight="8" tilecount="4096" columns="64">
+ <image source="galletcity.png" width="512" height="512"/>
+</tileset>
